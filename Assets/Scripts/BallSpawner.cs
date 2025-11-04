a54477dc70a71c903093f3cbaacfbc08079af3a7
LAPTOP-NIFDOGS3\sansg <cadenasu@shernet.sheridancollege.ca>
2025-11-04 13:03:04 -0500

    Add project files.

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public int ballSpawnCount = 100;
+    public float ballSpawnInterval = 0.3f;
+    public bool randomColours = true;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    IEnumerator Start()
+    {
+        for (int i = 0; i < ballSpawnCount; i++)
+        {
+            GameObject ball = Instantiate(ballPrefab, transform.position, Quaternion.identity, transform);
+
+            Rigidbody2D body2D = ball.GetComponent<Rigidbody2D>();
+            body2D.AddForce(randomColours.insideUnitCircle.normalized, ForceMode2D.Impulse);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
