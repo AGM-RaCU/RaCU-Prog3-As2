426d09ed8d91ef40376d85ea798448a5b6c18ebb
cadenasu <cadenasu@OA-S318-27>
2025-11-25 13:51:33 -0500

    Nothing is complete, but I have all my components!!!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,26 +4,66 @@ public class PlayerController : MonoBehaviour
 {
     public enum FacingDirection
     {
-        left, right
+        left = -1, right = 1
     }
+
+    [SerializeField] private Rigidbody2D body2D;
+
+    [Header("WalkProperties")]
+    public float maxSpeed = 5f;
+    public float accelerationTime = 0.5f;
+    public float deccelerationTime = 0.25f;
+
+    [Header("Jump Properties")]
+    public float apexHeight = 3.5f;
+    public float apexTime = 0.5f;
+    public LayerMask groundLayer;
+    public float groundCheckDistance = 0.55f;
+    public Vector2 groundCheckSize = new(0.75f, .2f);
+
+    private Vector2 velocity;
+    private float acceleration;
+    private float decceleration;
+
+    private float gravity;
+    private float jumpVel;
+
+    private Vector2 playerInput;
+    private bool jumpPressed = false;
 
     void Start()
     {
-        
+        acceleration = maxSpeed / accelerationTime;
+        decceleration = maxSpeed / deccelerationTime;
+
+        gravity = -2 * apexHeight / (apexTime * apexTime);
+        jumpVel = 2 * apexHeight / apexTime;
+
+        body2D.gravityScale = 0;
     }
 
     void Update()
     {
-        // The input from the player needs to be determined and
-        // then passed in the to the MovementUpdate which should
-        // manage the actual movement of the character.
-        Vector2 playerInput = new Vector2();
-        MovementUpdate(playerInput);
+        playerInput = new()
+        {
+            x = Input.GetAxisRaw("Horizontal"),
+            y = Input.GetButtonDown("Jump") ? 1 : 0
+        };
+
+        if (playerInput.y == 1) jumpPressed = true;
+    }
+
+    private void FixedUpdate()
+    {
+        MovementUpdate():
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
+        ProcessWalkInput();
+        ProcessJumpInput();
 
+        body2D.linearVelocity = velocity;
     }
 
     public bool IsWalking()
